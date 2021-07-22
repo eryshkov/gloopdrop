4e7a002d73ebacf91c53aaa04612faaee2a9eff9
Evgenii Ryshkov <eryshkov@gmail.com>
2021-07-22 23:37:30 +0300

    17 - node types

--- gloopdrop/GameViewController.swift
+++ gloopdrop/GameViewController.swift
@@ -14,30 +14,22 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        if let view = self.view as! SKView? { // Create the scene
+            let scene = GameScene(size: view.bounds.size)
+            // Set the scale mode to scale to fill the view window
+            scene.scaleMode = .aspectFill
+            // Set the background color
+            scene.backgroundColor = UIColor(red: 105/255, green: 157/255,
+                                            blue: 181/255,
+                                            alpha: 1.0)
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+            // Present the scene
+            view.presentScene(scene)
+            // Set the view options
+            view.ignoresSiblingOrder = false
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
         }
     }
 
@@ -46,11 +38,7 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
     }
 
     override var prefersStatusBarHidden: Bool {
