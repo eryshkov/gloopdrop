490c008e78810ab0de6041f005e3a8b8e6a195ce
Evgenii Ryshkov <eryshkov@gmail.com>
2021-10-21 00:59:53 +0300

    swipe to right

--- gloopdrop/GameScene.swift
+++ gloopdrop/GameScene.swift
@@ -7,6 +7,7 @@
 
 import SpriteKit
 import GameplayKit
+
 class GameScene: SKScene {
     let player = Player()
 
@@ -17,20 +18,28 @@ class GameScene: SKScene {
         background.zPosition = Layer.background.rawValue;
         background.position = CGPoint(x: 0, y: 0)
         addChild(background)
-        
+
         let foreground = SKSpriteNode(imageNamed: "foreground_1")
         foreground.anchorPoint = CGPoint.zero
         foreground.position = CGPoint(x: 0, y: 0)
         foreground.zPosition = Layer.foreground.rawValue
         addChild(foreground)
-        
-        player.position = CGPoint(x: size.width/2, y: foreground.frame.maxY)
+
+        player.position = CGPoint(x: size.width / 2, y: foreground.frame.maxY)
         player.setupConstraints(floor: foreground.frame.maxY)
         addChild(player)
         player.walk()
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipedRight(sender:)))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
     }
 
     // MARK: - TOUCH HANDLING
+
+    @objc func swipedRight(sender: UIGestureRecognizer) {
+        player.moveToPosition(pos: CGPoint(x: self.size.width, y: 0), direction: "R", speed: 1)
+    }
 
     func touchDown(atPoint pos: CGPoint) {
         if pos.x < player.position.x {
@@ -41,8 +50,9 @@ class GameScene: SKScene {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            self.touchDown(atPoint: t.location(in: self))
+        guard let touch = touches.first else {
+            return
         }
+        self.touchDown(atPoint: touch.location(in: self))
     }
 }
